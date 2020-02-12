0575e3cd9ce1b5a2157a084e5d5e54e27d6bbcbc
Francisco Perez <franciscotecnico2002@gmail.com>
2020-02-12 17:43:41 -0300

    listar clases disponibles para mañana y crear boton reservar

--- static/js/users.js
+++ static/js/users.js
@@ -3,25 +3,38 @@ function listarClasesDisponibles() {
     let agenda = JSON.parse(localStorage.getItem('agenda'))
 
     let tablaBody = ""
-
+    let hoy = moment(new Date()).add(1,'days')
+    let nombres_dias = new Array('Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado')
+    var nombres_meses = new Array('Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre')
+    /* var manana = new Date(hoy.getTime() + 24*60*60*1000) */
+    let dia = moment(hoy).date()
+    let mes = moment(hoy).month()
+    let anio = moment(hoy).year()
+    
+    console.log(dia);
+    console.log(mes);
+    console.log(anio);
+    
     for (let i = 0; i < agenda.length; i++) {
-
         
-        let hoy = new Date()
-        var manana=new Date(hoy.getTime() + 24*60*60*1000)
-        let nombres_dias = new Array('Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado')
-        let dias = agenda[i];
-        let newFecha = new Date(dias.fecha);
-        console.log(manana);
-        console.log(newFecha);
-
-        if (manana.getDate() == newFecha.getDate() ) {
+        let actividad = agenda[i];
+        let newFecha = new Date(actividad.fecha);
+        console.log(moment(newFecha).date());
+        console.log(moment(newFecha).month());
+        console.log(moment(newFecha).year());
+    
+       
+        if ( ( dia == moment(newFecha).date() ) && ( mes == moment(newFecha).month() ) && ( anio == moment(newFecha).year() ) ) {
             
+            console.log("holaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
             tablaBody += `<tr>
-                            <th scope="row">${dias.clase}</th>
-                            <td>${nombres_dias[newFecha.getDay()]} ${newFecha.getDate()} de ${newFecha.getFullYear()} </td>
-                            <td>${dias.turno}</td>
+                            <th scope="row">${actividad.clase}</th>
+                            <td>${nombres_dias[newFecha.getDay()]} ${newFecha.getDate()} de ${nombres_meses[newFecha.getMonth()+1]} de ${newFecha.getFullYear()} </td>
+                            <td>${actividad.turno}</td>
+                            <td><button type="button" class="btn btn-primary">Reservar</button></td>
                           </tr>`
+        } else {
+            console.log("no pasa nada");
         }
     
     }
